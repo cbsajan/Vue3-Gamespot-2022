914b3eb59d3a76b797be67fde03237f0e50581c2
C B Sajan <sajan@ckhathri.com>
2022-05-31 14:17:39 +0530

    Updated

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ import { createRouter, createWebHistory, START_LOCATION } from 'vue-router';
 // COMPONENTS
 import Home from './components/Home/Index_comp.vue';
 import Article_comp from './components/Articles/Article_comp.vue';
+import Signin from './components/User/Signin_comp.vue';
 
 
 const routes = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Home, name: 'home' },
-        { path: '/article/:id', component: Article_comp, name: 'article' }
+        { path: '/article/:id', component: Article_comp, name: 'article' },
+        { path: '/signin', component: Signin, name: 'signin' }
     ]
 })
 
