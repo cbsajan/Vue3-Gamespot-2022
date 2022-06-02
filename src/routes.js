4f88272fcae94d577ade899526137cd116f64b96
C B Sajan <sajan@ckhathri.com>
2022-06-02 12:01:14 +0530

    Update in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { createRouter, createWebHistory, START_LOCATION } from 'vue-router';
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 // COMPONENTS
 import Home from './components/Home/Index_comp.vue';
 import Article_comp from './components/Articles/Article_comp.vue';
@@ -15,6 +15,23 @@ const routes = createRouter({
         { path: '/signin', component: Signin, name: 'signin' }
     ]
 })
+const auth = getAuth();
 
+
+routes.beforeEach((to, from, next) => {
+    if (from === START_LOCATION) {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                console.log(user, 'autosign in')
+            } else {
+                console.log(user, 'not autosign in')
+            }
+        })
+        next();
+    } else {
+        console.log('other')
+        next()
+    }
+})
 
 export default routes;
