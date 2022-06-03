a7c74c0ad9a714b102e232ef8b225d63dc2e1116
C B Sajan <sajan@ckhathri.com>
2022-06-03 15:58:56 +0530

    Updates

---
+++ src/components/Tools/schemas.js
@@ -0,0 +1,17 @@
+
+import * as yup from 'yup';
+
+
+const addArticleSchema = {
+    game: yup.string(),
+    title: yup.string(),
+    excerpt: yup.string(),
+    editor: yup.string(),
+    rating: yup.string(),
+    img: yup.string()
+}
+
+
+export {
+    addArticleSchema
+}
