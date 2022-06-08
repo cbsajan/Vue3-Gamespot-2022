eae306d19b88d67f3fd41bc160cda96a8b89a196
C B Sajan <sajan@ckhathri.com>
2022-06-08 14:17:37 +0530

    30-Profile-update-the-form

--- src/components/User/Dashboard/Pages/user_profile.vue
+++ src/components/User/Dashboard/Pages/user_profile.vue
@@ -1,12 +1,61 @@
 <template>
     <dashboard-title title="Profile" />
+    <div v-if="userData">
+        <Form @submit="onSubmit" :validation-schema="formSchema">
+            <div class="form-group">
+                <Field name="firstname" v-model="userData.firstname" v-slot="{ field, errors, errorMessage }">
+                    <form-element :field="field" :errors="errors" :errorMessage="errorMessage" element="input"
+                        type="text" placeholder="First Name" />
+                </Field>
+            </div>
+            <br>
+            <div class="form-group">
+                <Field name="lastname" v-model="userData.lastname" v-slot="{ field, errors, errorMessage }">
+                    <form-element :field="field" :errors="errors" :errorMessage="errorMessage" element="input"
+                        type="text" placeholder="Last Name" />
+                </Field>
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary">
+                Update User
+            </button>
+        </Form>
+    </div>
 </template>
 
 <script>
 import DashboardTitle from '../../../Utils/dashTitle.vue';
+import { Field, Form } from 'vee-validate';
+import FormElement from '../../../Utils/form_elements.vue';
+import * as yup from 'yup'
 export default {
     components: {
-        DashboardTitle
+        DashboardTitle,
+        FormElement,
+        Form,
+        Field
+    },
+    data() {
+        return {
+
+            formSchema: {
+                firstname: yup.string(),
+                lastname: yup.string()
+            }
+        }
+    },
+    computed: {
+        userData() {
+            return JSON.parse(JSON.stringify(this.$store.getters['auth/getUserData']));
+        }
+    },
+    methods: {
+        onSubmit(values) {
+
+
+            console.log(values)
+
+        },
     }
 }
 </script>
